b9f2800bec16233fa406db2dd6c4439a7d244b30
Hegazy <h3g4zy@gmail.com>
2023-10-20 20:42:04 +0300

    18489

--- vv.c
+++ vv.c
@@ -8,21 +8,28 @@
  *
  * Return: 1 if it's a chain delimiter, 0 otherwise.
  */
-int is_chain(info_t *info, char *buf, size_t *p) {
+int is_chain(info_t *info, char *buf, size_t *p)
+{
     size_t j = *p;
 
-    if (buf[j] == '|' && buf[j + 1] == '|') {
+    if (buf[j] == '|' && buf[j + 1] == '|')
+    {
         buf[j] = 0;
         j++;
         info->cmd_buf_type = CMD_OR;
-    } else if (buf[j] == '&' && buf[j + 1] == '&') {
+    }
+    else if (buf[j] == '&' && buf[j + 1] == '&')
+    {
         buf[j] = 0;
         j++;
         info->cmd_buf_type = CMD_AND;
-    } else if (buf[j] == ';') {
+    } else if (buf[j] == ';')
+    {
         buf[j] = 0;
         info->cmd_buf_type = CMD_CHAIN;
-    } else {
+    }
+    else
+    {
         return 0;
     }
     *p = j;
@@ -39,17 +46,22 @@ int is_chain(info_t *info, char *buf, size_t *p) {
  *
  * Return: Void.
  */
-void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len) {
+void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
+{
     size_t j = *p;
 
-    if (info->cmd_buf_type == CMD_AND) {
-        if (info->status) {
+    if (info->cmd_buf_type == CMD_AND)
+    {
+        if (info->status)
+        {
             buf[i] = 0;
             j = len;
         }
     }
-    if (info->cmd_buf_type == CMD_OR) {
-        if (!info->status) {
+    if (info->cmd_buf_type == CMD_OR)
+    {
+        if (!info->status)
+        {
             buf[i] = 0;
             j = len;
         }
@@ -69,18 +81,22 @@ int replace_alias(info_t *info) {
     list_t *node;
     char *p;
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
+    {
         node = node_starts_with(info->alias, info->argv[0], '=');
-        if (!node) {
+        if (!node)
+        {
             return 0;
         }
         free(info->argv[0]);
         p = _strchr(node->str, '=');
-        if (!p) {
+        if (!p)
+        {
             return 0;
         }
         p = _strdup(p + 1);
-        if (!p) {
+        if (!p)
+        {
             return 0;
         }
         info->argv[0] = p;
@@ -94,25 +110,30 @@ int replace_alias(info_t *info) {
  *
  * Return: 1 if replaced, 0 otherwise.
  */
-int replace_vars(info_t *info) {
+int replace_vars(info_t *info)
+{
     int i = 0;
     list_t *node;
 
     for (i = 0; info->argv[i]; i++) {
-        if (info->argv[i][0] != '$' || !info->argv[i][1]) {
+        if (info->argv[i][0] != '$' || !info->argv[i][1])
+        {
             continue;
         }
 
-        if (!_strcmp(info->argv[i], "$?")) {
+        if (!_strcmp(info->argv[i], "$?"))
+        {
             replace_string(&(info->argv[i]), _strdup(convert_number(info->status, 10, 0));
             continue;
         }
-        if (!_strcmp(info->argv[i], "$$")) {
+        if (!_strcmp(info->argv[i], "$$"))
+        {
             replace_string(&(info->argv[i]), _strdup(convert_number(getpid(), 10, 0)));
             continue;
         }
         node = node_starts_with(info->env, &info->argv[i][1], '=');
-        if (node) {
+        if (node)
+        {
             replace_string(&(info->argv[i]), _strdup(_strchr(node->str, '=') + 1));
             continue;
         }
@@ -128,7 +149,8 @@ int replace_vars(info_t *info) {
  *
  * Return: 1 if replaced, 0 otherwise.
  */
-int replace_string(char **old, char *new) {
+int replace_string(char **old, char *new)
+{
     free(*old);
     *old = new;
     return 1;
